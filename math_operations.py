072b801baca0fee8fdc1639432e6640fc28bf2e8
patildhruv0879 <dhruvskp@gmail.com>
2025-12-12 15:14:52 +0530

    initial commit

---
+++ math_operations.py
@@ -0,0 +1,6 @@
+num1= int(input("Enter the first number:"))
+num2= int(input("Enter the second number:"))
+print("Addition:",num1+num2)
+print("Subraction:",num1-num2)
+print("Multipliction:",num1*num2)
+print("Division:",num1/num2)
